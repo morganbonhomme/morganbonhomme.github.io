2353a8a0e661d9cb0c6b3e7928d2fe79e7ee3f55
morganbonhomme <bonhomme.morgan@gmail.com>
2020-09-03 14:25:38 +0200

    Updates

---
+++ precache-manifest.188d0ba1b22e32f314c2957a5f546b19.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65bc62d65a1043ebe8aa0fb66f886059",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76a3336c05ed90425325",
+    "url": "/static/css/main.6dd2f69e.chunk.css"
+  },
+  {
+    "revision": "20c1c338b7219862a9c2",
+    "url": "/static/js/2.756ee818.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.756ee818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a3336c05ed90425325",
+    "url": "/static/js/main.225da2eb.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "dc3a44e903f8919b3337aa20977733bc",
+    "url": "/static/media/ID.dc3a44e9.jpg"
+  },
+  {
+    "revision": "515845043e3297ddffc260f2163414c0",
+    "url": "/static/media/etchdemo.51584504.gif"
+  },
+  {
+    "revision": "e6e929286a88503b5b7629bb0e0495ac",
+    "url": "/static/media/github.e6e92928.png"
+  },
+  {
+    "revision": "6e0930c771a8c862440e4927fc94cef7",
+    "url": "/static/media/linkedin.6e0930c7.png"
+  },
+  {
+    "revision": "424ae304cc67732bf5dd9cc30e96f711",
+    "url": "/static/media/messagedemo.424ae304.png"
+  },
+  {
+    "revision": "4b998a407e38547b5a924ff60270b94c",
+    "url": "/static/media/nytdemo.4b998a40.png"
+  },
+  {
+    "revision": "0156bf102fb864df921abc8e42c9b40d",
+    "url": "/static/media/responsivedesigndemo.0156bf10.gif"
+  },
+  {
+    "revision": "22632c1c740838aa6e0ae3b0d83dbec0",
+    "url": "/static/media/salary1.22632c1c.gif"
+  },
+  {
+    "revision": "385f2f6e5b2c8e80b1bfeb851c3aa685",
+    "url": "/static/media/salary2.385f2f6e.png"
+  },
+  {
+    "revision": "7c25a9ace9afe116b6637a47982b9a4f",
+    "url": "/static/media/salary3.7c25a9ac.png"
+  },
+  {
+    "revision": "e15ed0a1b34374d6049392446d1aba16",
+    "url": "/static/media/salarysimu.e15ed0a1.svg"
+  },
+  {
+    "revision": "ed7f10514e12a5cd1b4d1d9641d0ac70",
+    "url": "/static/media/secretblogdemo.ed7f1051.png"
+  },
+  {
+    "revision": "20f526872bed98e173d6ad68b42f0552",
+    "url": "/static/media/tictactoedemo.20f52687.gif"
+  },
+  {
+    "revision": "6853b8221b0f2b8c9246365abaaa643c",
+    "url": "/static/media/weatherdemo.6853b822.png"
+  }
+]);
